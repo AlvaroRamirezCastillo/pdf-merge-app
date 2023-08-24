286ee65ff7ec46723daca6d10fe7c846e29f707d
Alvaro Ramirez Castillo <alvaro@MacBook-Pro-de-Alvaro.local>
2023-08-23 20:47:43 -0500

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { PDFDocument, rgb } from 'pdf-lib';
 
 function App() {
+  const [pdfFiles, setPdfFiles] = useState([]);
+  const [mergedPdf, setMergedPdf] = useState(null);
+
+  const handleFileChange = (event) => {
+    const newFiles = Array.from(event.target.files);
+    setPdfFiles(newFiles);
+  };
+
+  const mergePDFs = async () => {
+    try {
+      const mergedPdfDoc = await PDFDocument.create();
+      for (const pdfFile of pdfFiles) {
+        const pdfBytes = await pdfFile.arrayBuffer();
+        const pdfDoc = await PDFDocument.load(pdfBytes);
+
+        const pages = await mergedPdfDoc.copyPages(pdfDoc, pdfDoc.getPageIndices());
+        pages.forEach((page) => {
+          mergedPdfDoc.addPage(page);
+        });
+      }
+
+      const mergedPdfBytes = await mergedPdfDoc.save();
+      setMergedPdf(new Blob([mergedPdfBytes], { type: 'application/pdf' }));
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>PDF Merger</h1>
+      <input type="file" multiple onChange={handleFileChange} />
+      <button onClick={mergePDFs}>Merge PDFs</button>
+
+      {mergedPdf && (
+        <div>
+          <h2>Merged PDF</h2>
+          <iframe
+            title="Merged PDF Viewer"
+            src={URL.createObjectURL(mergedPdf)}
+            width="600"
+            height="400"
+          />
+          <a href={URL.createObjectURL(mergedPdf)} download="merged.pdf">
+            Download Merged PDF
+          </a>
+        </div>
+      )}
     </div>
   );
 }
